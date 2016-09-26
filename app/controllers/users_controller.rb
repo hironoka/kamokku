c2d5f9c14ee828244f3fa30dc3960d6b47b4acab
hirono <hotarak@gmail.com>
2016-09-26 23:30:02 +0900

    second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:edit, :show, :update]
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to :root
+      alert_user_edit_successe_message
+    else
+      redirect_to edit_user_path
+      alert_user_edit_error_message
+    end
+  end
+
+ private
+  def user_params
+    params.require(:user).permit(:name, :email,:member, :profile, :works, :avatar)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
